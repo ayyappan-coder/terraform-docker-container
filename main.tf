386a815e6bc8375b58951392d912bb675dfa6cca
ayyappan-coder <150893648+ayyappan-coder@users.noreply.github.com>
2024-05-22 16:23:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
